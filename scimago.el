a0f0aa4aeb483b9b97833bcbc1e0e54cff4067cc
Omar Antolín <omar.antolin@gmail.com>
2025-05-04 23:05:11 -0600

    Move all data to subdirectory

--- scimago.el
+++ scimago.el
@@ -4,7 +4,7 @@
 
 (defcustom scimago-data-file
   (expand-file-name
-   "mathsjr.lisp"
+   "data/mathsjr.lisp"
    (file-name-directory (locate-library "scimago")))
   "Location of files containing Scimago data."
   :group 'scimago
